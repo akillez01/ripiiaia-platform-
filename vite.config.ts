acb92454cdfd7e229216c75ecd6f97f93d7858de
Akilles01 <achilles.oliveira.souza@gmail.com>
2025-05-27 02:58:33 -0400

    ajustes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/ripiiaia-nexus-platform/',
+  base: mode === 'development' ? '/' : '/ripiiaia-nexus-platform/',
   server: {
     host: "::",
     port: 8080,

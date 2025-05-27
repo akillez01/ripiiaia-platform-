d0c68754fc565aca6104b92de8eff5d672d1431b
Akilles01 <achilles.oliveira.souza@gmail.com>
2025-05-27 01:54:31 -0400

    vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/ripiiaia-nexus-platform/',
   server: {
     host: "::",
     port: 8080,
